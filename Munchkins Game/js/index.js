8377f1a332ac7278fe1554aeea86818b53185111
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-07-13 19:13:41 +0100

    update id's and js

--- Munchkins Game/js/index.js
+++ Munchkins Game/js/index.js
@@ -7,9 +7,7 @@ console.log("Hello World");
         let heroImage = document.getElementById("hero-image");
 
         if (selectedClass === "1") { // Cleric
-            heroImage.src = "images/priest_2822364 (3).png";
-            console.log("selectedClass");
-            
+            heroImage.src = "images/priest_2822364 (3).png";            
         } else if (selectedClass === "2") { // Thief
             heroImage.src = "";
         } else if (selectedClass === "3") { // Warrior
@@ -26,8 +24,19 @@ console.log("Hello World");
         let strength = level + weapons;
         document.getElementById("strength").value = strength;
     }
-    
 
+// card color
+    function changeCardColor() {
+        let cardColor = document.getElementById("cardColorInput").value;
+        let card = document.getElementById("card");
+        card.style.backgroundColor = cardColor;
+    }
 
-
+// cards per player 
+    function calculateCardsPerPlayer() {
+        let players = parseInt(document.getElementById("players").value) || 0;
+        let cards = parseInt(document.getElementById("cards").value) || 0;
+        let cardsPerPlayer = cards / players;
+        document.getElementById("cardsPerPlayer").value = cardsPerPlayer;
+    }
 
