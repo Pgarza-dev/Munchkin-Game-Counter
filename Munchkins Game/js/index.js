609fae6182b7286fc4e72ecbb296812cc6154692
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-07-13 13:00:27 +0100

    add images & class selection in js

--- Munchkins Game/js/index.js
+++ Munchkins Game/js/index.js
@@ -1,1 +1,24 @@
 console.log("Hello World");
+// choosing race
+
+// chooseing classes
+    function changeHeroImage() {
+        let selectedClass = document.getElementById("floatingSelectClass").value;
+        let heroImage = document.getElementById("hero-image");
+
+        if (selectedClass === "1") { // Cleric
+            heroImage.src = "images/priest_2822364 (3).png";
+            console.log("selectedClass");
+            
+        } else if (selectedClass === "2") { // Thief
+            heroImage.src = "";
+        } else if (selectedClass === "3") { // Warrior
+            heroImage.src = "images/swordsman_2822374.png";
+        } else if (selectedClass === "4") { // Wizard
+            heroImage.src = "images/wizard_2822377.png";
+        }
+    }
+
+
+
+
