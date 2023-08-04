57d691a5b021e1fddd5763e725c80fc7193746e2
Pgarza-dev <84074301+Pgarza-dev@users.noreply.github.com>
2023-08-04 13:00:25 +0100

    add dice function

--- Munchkins Game/js/battle.js
+++ Munchkins Game/js/battle.js
@@ -1,1 +1,80 @@
 console.log("battle.js loaded");
+
+function createDice(number) {
+	const dotPositionMatrix = {
+		1: [
+			[50, 50]
+		],
+		2: [
+			[20, 20],
+			[80, 80]
+		],
+		3: [
+			[20, 20],
+			[50, 50],
+			[80, 80]
+		],
+		4: [
+			[20, 20],
+			[20, 80],
+			[80, 20],
+			[80, 80]
+		],
+		5: [
+			[20, 20],
+			[20, 80],
+			[50, 50],
+			[80, 20],
+			[80, 80]
+		],
+		6: [
+			[20, 20],
+			[20, 80],
+			[50, 20],
+			[50, 80],
+			[80, 20],
+			[80, 80]
+		]
+	};
+
+    const dice = document.createElement("div");
+    dice.classList.add("dice");
+
+    for (const dotPosition of dotPositionMatrix[number]) {
+        const dot = document.createElement("div");
+        dot.classList.add("dice-dot");
+        dot.style.setProperty("--top", dotPosition[0] + "%");
+        dot.style.setProperty("--left", dotPosition[1] + "%");
+        
+        dice.appendChild(dot);
+    }
+
+    return dice;
+}
+
+function randomizedDice(diceContainer, numberOfDice) {
+    diceContainer.innerHTML = "";
+
+    for (let i = 0; i < numberOfDice; i++) {
+        const dice = createDice(Math.floor(Math.random() * 6) + 1);
+        diceContainer.appendChild(dice);
+    }
+};
+
+const NUMBER_OF_DICE = 1;
+const diceContainer = document.querySelector(".dice-container");
+const btnRollDice = document.querySelector(".btn-roll-dice");
+
+randomizedDice(diceContainer, NUMBER_OF_DICE);
+
+btnRollDice.addEventListener("click", () => {
+    // randomizedDice(diceContainer, NUMBER_OF_DICE);
+    const interval = setInterval(() => {
+        randomizedDice(diceContainer, NUMBER_OF_DICE);
+    }, 50);
+
+    setTimeout(() => {
+        clearInterval(interval);
+    }, 800);
+});
+
